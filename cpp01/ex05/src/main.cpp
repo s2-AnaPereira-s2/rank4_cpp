1e1994307b5f9d8a1ec6433d201953178e25da0c
Ana Paula Pereira <ana.she.po@gmail.com>
2026-03-18 22:32:06 +0100

    finished ex05

---
+++ cpp01/ex05/src/main.cpp
@@ -0,0 +1,16 @@
+#include "Harl.hpp"
+
+int main()
+{
+    Harl harl;
+
+    std::cout << "---------DEBUG--------" << std::endl;
+    harl.complain("debug");
+    std::cout << "---------INFO---------" << std::endl;
+    harl.complain("info");
+    std::cout << "---------WARNING-------" << std::endl;
+    harl.complain("warning");
+    std::cout << "---------ERROR---------" << std::endl;
+    harl.complain("error");
+    return 0;
+}
